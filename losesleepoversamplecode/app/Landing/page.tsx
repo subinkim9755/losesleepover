d06486e62ea8ddff02dcb9b5e215d1c7f4cedcb2
unknown <a01072669755@gmail.com>
2026-08-16 02:17:17 +0900

    ProjectSet

---
+++ losesleepoversamplecode/app/Landing/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/dist/client/link";
+
+export default function Home() {
+  return (
+    
+      <div>
+        <Link
+         href="/Landing">
+          <button>About</button>
+        </Link>
+        ㅎㅇ
+      </div>
+      
+
+  );
+}
